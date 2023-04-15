f996f87e40772cca5aabcc840ba4d998be950e40
tarikhusin <tarik.id.10@hotmail.com>
2023-04-15 23:46:57 +0300

    star finder working

--- star_finder.py
+++ star_finder.py
@@ -4,24 +4,26 @@ import cv2
 from PIL import Image
 from pillow_heif import register_heif_opener
 
-register_heif_opener()
 
 
-def load_image(path, size):
-    image_gray = np.array(Image.open(path).convert('L'))
-    # image_gray = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
-    return cv2.resize(image_gray, (size, size), interpolation=cv2.INTER_LINEAR)
+# def load_image(path, size= 600 ,  grayscale=False):
+#     image = np.array(Image.open(path))
+
+#     if grayscale:
+#         image= cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+#     return cv2.resize(image, (size, size), interpolation=cv2.INTER_LINEAR)
 
 
-def has_intersecting_cicle(circles, circle):
-    '''check if circle intersect with another circle'''
+# def has_intersecting_cicle(circles, circle):
+#     '''check if circle intersect with another circle'''
 
-    x1, y1, radius1 = circle
-    for x2, y2, radius2, b in circles:
-        dist = sqrt((x2 - x1)**2 + (y2 - y1)**2)
-        if dist < radius1 + radius2:
-            return True
-    return False
+#     x1, y1, radius1 = circle
+#     for x2, y2, radius2, b in circles:
+#         dist = sqrt((x2 - x1)**2 + (y2 - y1)**2)
+#         if dist < radius1 + radius2:
+#             return True
+#     return False
 
 
 def detect_stars(img: np.ndarray):
@@ -32,14 +34,13 @@ def detect_stars(img: np.ndarray):
 
 
 # main runing function
-def get_stars(path, size):
-    image = load_image(path, size)
+def get_stars(image, size):
     circles = detect_stars(image)
 
     circles = np.uint16(np.around(circles))
     circles_cordinates = []
     for i in circles[0, :]:
-        if i[0] < size and i[1] < size:
+        if i[0] < size[0] and i[1] < size[1]:
             circles_cordinates.append(
                 (i[0], i[1], i[2] + 5,  image[i[0], i[1]]))
     return circles_cordinates
@@ -47,21 +48,28 @@ def get_stars(path, size):
 
 # run main function
 if __name__ == '__main__':
-    path1 = './imgs/IMG_3053.HEIC'
-    path2 = './imgs/IMG_3054.HEIC'
+    register_heif_opener()
 
-    img1 = load_image(path1, 600)
-    img2 = load_image(path2, 600)
+    path1 = './imgs/IMG_3066.JPG'
+    path2 = './imgs/IMG_3067.JPG'
+    size = (600,600)
 
-    stars1 = get_stars(path1, 600)
-    stars2 = get_stars(path2, 600)
+    img1 = np.array(Image.open(path1).resize(size))
+    img2 = np.array(Image.open(path2).resize(size))
+    
+    img1_gray= cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+    img2_gray= cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+
+
+    stars1 = get_stars(img1_gray, size)
+    stars2 = get_stars(img2_gray, size)
 
     # draw the outer circle
     for i in stars1:
-        img1 = cv2.circle(img1, (i[0], i[1]), i[2], (255, 0, 0), 1, 0)
+        img1 = cv2.circle(img1, (i[0], i[1]), i[2], (255, 0, 255), 1, 0)
 
     for i in stars2:
-        img2 = cv2.circle(img2, (i[0], i[1]), i[2], (255, 0, 0), 1, 0)
+        img2 = cv2.circle(img2, (i[0], i[1]), i[2], (255, 0, 255), 1, 0)
 
     cv2.imshow('detected circles 1', img1)
     cv2.imshow('detected circles 2', img2)
